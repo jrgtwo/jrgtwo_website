2f78cd542454988512d81b31c1c486decb89e3bd
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-05-06 17:08:50 -0700

    fix: blog layout header fix

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -18,7 +18,7 @@ export default async function BlogLayout({ children }: PropsWithChildren) {
   return (
     <>
       <section className="py-full-section">
-        <h2 className="hidden md:block text-center text-5xl font-[Cal_Sans]" style={{ 'textShadow': '#860202 0px 3px 0px, white 0px 2px 0px' }}>Jonathan's Explorations 🧑‍🚀 🚀</h2>
+        <h2 className=" text-center text-5xl font-[Cal_Sans]" style={{ 'textShadow': '#860202 0px 3px 0px, white 0px 2px 0px' }}>Jonathan's Explorations 🧑‍🚀 🚀</h2>
         <Separator className="my-8" />
         <section className="flex flex-col-reverse  lg:flex-row gap-8">
           <section className="w-[310px] px-4 lg:block">
