6760f67c0c9b2ca53e66e85af5e174955f11865f
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-05-09 16:11:14 -0700

    chore: cleaning up deprecated files

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_DB_URL
-const supabaseKey = process.env.NEXT_PUBLIC_DB_KEY
-const supabaseClient = (supabaseKey && supabaseUrl) && createClient(supabaseUrl, supabaseKey)
-
-export { supabaseClient }
