ed105537d81439360f5bfb0aac11744a87edde40
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-05-13 16:17:14 -0700

    fix: removing creation date from post list

--- src/components/blog/page/BlogList.tsx
+++ src/components/blog/page/BlogList.tsx
@@ -14,7 +14,7 @@ export default function BlogList({ data }) {
           <div>
             <Link href={`/blog/post/${post.id}`}>
               <h3 className="font-[Cal_Sans] text-2xl">{post.title}</h3>
-              <p className="font-[Roboto_Condensed] text-sm">{new Date(post.created_at).toLocaleDateString()}</p>
+              {/* <p className="font-[Roboto_Condensed] text-sm">{new Date(post.created_at).toLocaleDateString()}</p> */}
               <div className="flex gap-4 mt-4 p-4 items-center">
                 <Image
                   src={post.thumbnail}
